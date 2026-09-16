3794163184ae4237748e070c0e7912bbbdd346e2
egonar-code <moussacarter@gmail.com>
2026-09-16 06:27:26 +0000

    Persister la langue avant rechargement

--- apps/web/language-switch.js
+++ apps/web/language-switch.js
@@ -2,6 +2,7 @@
   'use strict';
   document.addEventListener('change', function (event) {
     if (event.target && event.target.matches('[data-language-selector]')) {
+      localStorage.setItem('egonar_language', event.target.value === 'en' ? 'en' : 'fr');
       window.location.reload();
     }
   }, true);
