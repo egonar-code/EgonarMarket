26ca9c7d753a0359164e325f70b67e8e86295390
egonar-code <moussacarter@gmail.com>
2026-09-17 23:33:02 +0000

    Retirer une migration fournisseur créée par erreur de la branche par défaut

--- db/migrations/20260917_supplier_password.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE suppliers
-  ADD COLUMN IF NOT EXISTS password_hash TEXT;
-
-UPDATE suppliers
-SET password_hash = '$2a$10$7EqJtq98hPqEX7fNZaFWoO7c1u2fVQ0r8Qv6zY8GmY8mV4c7mXj9e'
-WHERE password_hash IS NULL;
-
-ALTER TABLE suppliers
-  ALTER COLUMN password_hash SET NOT NULL;
